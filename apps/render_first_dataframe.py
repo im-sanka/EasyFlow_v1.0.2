41c08a15afc9b64fe758d491dc80ff0771674f95
im-sanka <immanuel.sanka@taltech.ee>
2022-04-27 10:08:36 +0300

    making all label is string

--- apps/render_first_dataframe.py
+++ apps/render_first_dataframe.py
@@ -1,12 +1,10 @@
-import math
-import re
 import streamlit
 
 def first_dataframe(data_frame):
     try:
         data_frame['Intensity'] = data_frame.iloc[:, 2]
         data_frame['Volume'] = data_frame.iloc[:, 1]
-        data_frame['Label']  = data_frame.iloc[:, 0] #First column
+        data_frame['Label']  = data_frame.iloc[:, 0].astype(str) #First column
         #data_frame['Radian'] = data_frame['Diameter'] / 2
         # Spheroid = (4 / 3) * (math.pi * data_frame['Radian'] ** 3) / 1000000
         # Ellipsoid = (4 / 3) * (
