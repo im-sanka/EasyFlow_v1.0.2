af2073df8e74dedafd06a223ff9a51549bbb009d
Danber <danielbereznoi@gmail.com>
2022-11-22 20:51:09 +0200

    Can now save settings in database, moved get_user_id function from droplet_data_service to database_service for convenience
    Added form for saving settings, can be given name and description. Created a function in analysis_settings_service that creates default settings for analysis, threshold calculation is also moved there.
    
    Closes Issue: https://github.com/im-sanka/EasyFlow_v1.0.2/issues/12

--- apps/render_threshold.py
+++ apps/render_threshold.py
@@ -4,6 +4,7 @@ def render_threshold(column1, data_frame):
     def_threshold = streamlit.session_state['analysis_settings']['body']['threshold']
     threshold: float = streamlit.number_input("Put your threshold here:", def_threshold,
                                             format="%.4f")
+
     streamlit.session_state['analysis_settings']['threshold'] = threshold
     # Classification based on the threshold
     data_frame.loc[data_frame['Intensity'] > threshold, 'Classification'] = 'Positive'
